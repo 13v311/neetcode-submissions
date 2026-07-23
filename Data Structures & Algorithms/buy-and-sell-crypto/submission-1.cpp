00e0a818f4289629af4701f3be883ac4e480b6aa
Levell K <levellkensey06@gmail.com>
2026-07-23 14:57:12 -0500

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,19 @@
+//this is an even easier version of the container with most water problem, u only need 1 ptr
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+       int maxProfit = 0;
+       int p = 0;
+       for(size_t i = 1; i < prices.size(); i++) {
+            int currProfit = prices[i] - prices[p];
+            if(currProfit > maxProfit) {
+                maxProfit = currProfit;
+            }
+
+            if(prices[p] > prices[i]) {
+                p = i;
+            }
+       }
+       return maxProfit;
+    }
+};
